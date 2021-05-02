07af66c1e80756945640944c8a7d29a1ca420cca
Harold Perez <haroldjose2903@gmail.com>
2021-05-02 00:23:29 -0300

    add new test for unknown word and error return

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,26 @@ package maps
 import "testing"
 
 func TestDictionary(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	t.Run("with known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
+
+	t.Run("with unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
