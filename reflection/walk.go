24d1d807d3ef3c70efa855a9d2655a756165a58c
Harold Perez <harcarta@amazon.com>
2021-05-05 17:48:38 -0300

    add cases for when theres multiple types on the struct

--- reflection/walk.go
+++ reflection/walk.go
@@ -3,11 +3,13 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	// fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
 	val := reflect.ValueOf(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fn(field.String())
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
 	}
 }
