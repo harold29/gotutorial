d4ac1143ebffacf366cc435bf0c91921eaab58cb
Harold Perez <harcarta@amazon.com>
2021-05-05 19:08:40 -0300

    add additional test with new helper to prevent flaky tests

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	// t.Fatal("not implemented")
 	cases := []struct {
 		Name          string
 		Input         interface{}
@@ -67,14 +66,6 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
-		{
-			"Maps",
-			map[string]string{
-				"Foo": "Bar",
-				"Baz": "Boz",
-			},
-			[]string{"Bar", "Boz"},
-		},
 	}
 
 	for _, test := range cases {
@@ -90,24 +81,35 @@ func TestWalk(t *testing.T) {
 		})
 	}
 
-	// expected := "Chris"
-	// var got []string
-	//
-	// x := struct {
-	//   Name string
-	// }{expected}
-	//
-	// walk(x, func(input string) {
-	//   got = append(got, input)
-	// })
-	//
-	// if len(got) != 1 {
-	//   t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
-	// }
-	//
-	// if got[0] != expected {
-	//   t.Errorf("got %q want %q", got[0], expected)
-	// }
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didnt", haystack, needle)
+	}
 }
 
 type Profile struct {
