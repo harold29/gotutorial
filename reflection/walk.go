cb17ae68d5dc87777aabae5ac32578960ab5aadf
Harold Perez <harcarta@amazon.com>
2021-05-05 17:36:20 -0300

    fix method to cases when theres multiple cases

--- reflection/walk.go
+++ reflection/walk.go
@@ -5,6 +5,9 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	// fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 }
