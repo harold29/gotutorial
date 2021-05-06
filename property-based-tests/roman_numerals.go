3ce533ef0c7e5b3277180c75634792a447feae80
Harold Perez <haroldjose2903@gmail.com>
2021-05-06 15:36:25 -0300

    refactor code to handle the byte array

--- property-based-tests/roman_numerals.go
+++ property-based-tests/roman_numerals.go
@@ -25,7 +25,8 @@ var allRomanNumerals = RomanNumerals{
 
 type RomanNumerals []RomanNumeral
 
-func (r RomanNumerals) ValueOf(symbol string) int {
+func (r RomanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
 	for _, s := range r {
 		if s.Symbol == symbol {
 			return s.Value
@@ -56,18 +57,14 @@ func ConvertToArabic(roman string) int {
 		symbol := roman[i]
 
 		if couldBeSubstractive(i, symbol, roman) {
-			nextSymbol := roman[i+1]
-
-			potentialNumber := string([]byte{symbol, nextSymbol})
-
-			if value := allRomanNumerals.ValueOf(potentialNumber); value != 0 {
+			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
 				total += value
 				i++
 			} else {
 				total++
 			}
 		} else {
-			total += allRomanNumerals.ValueOf(string([]byte{symbol}))
+			total += allRomanNumerals.ValueOf(symbol)
 		}
 	}
 
